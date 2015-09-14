be8fbae18f33a691707942be399cde2cc5d4db2d
mdnu <mark@sdentertainment.ca>
2015-09-13 23:31:50 -0400

    Added 'MinOfTwo' program.

---
+++ ab04_MinOfTwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ab04_MinOfTwo {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		long[] array = new long[size];
+		for (int i=0;i<size;i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			array[i] = (a < b) ? a : b;
+		}
+		for (int i=0;i<size-1;i++) {
+			System.out.print(array[i]+" ");
+		} System.out.print(array[size - 1]);
+	}
+	
+}
