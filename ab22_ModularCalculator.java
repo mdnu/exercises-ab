a99b9de007a18f0ec42e44bcce4f2ae1c55c0f81
mdnu <mark@sdentertainment.ca>
2015-09-18 02:20:13 -0400

    Added ModularCalculator program.

---
+++ ab22_ModularCalculator.java
@@ -0,0 +1,45 @@
+import java.math.BigInteger;
+import java.io.*;
+
+class ab22_ModularCalculator {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = 
+		new BufferedReader(new InputStreamReader(System.in));
+		BigInteger input = new BigInteger(in.readLine());
+		char operation;
+		String temp;
+		
+		while ((temp = in.readLine()) != null && temp.trim().length() > 0) {
+			BigInteger step = new BigInteger(temp.substring(2));
+			operation = temp.charAt(0);
+
+			switch(operation) {
+				case ('*'): {
+					input = input.multiply(step);
+					break;
+				}
+				case ('+'): {
+					input = input.add(step);
+					break;
+				}
+				case('%'): {
+					input = input.mod(step);
+					break;
+				}
+			}
+			
+		}
+		
+		System.out.print(input);
+	}
+	
+}
+
+// setup:
+// input data: initial integer number in first line.
+// one or more lines describing the operations, in form
+// 'sign value' where sign is either + or * and value
+// is an integer. last line in the same form, but with
+// % instead, and the number by which the result should
+// be divided by to get the remainder.
