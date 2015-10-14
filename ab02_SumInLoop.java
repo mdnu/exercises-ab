35512d4d7907d08f3543c44a1d247853265b6723
λδ <mdnu@users.noreply.github.com>
2015-10-13 23:46:17 -0400

    Code cleaning.

--- ab02_SumInLoop.java
+++ ab02_SumInLoop.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ab02_SumInLoop {
 	
-	public int sum(int[] array, int size) {
+	public static int sum(int[] array, int size) {
 		int sum = 0;
 		for (int i=0;i<size;i++) {
 			sum+=array[i];
@@ -11,16 +11,16 @@ public class ab02_SumInLoop {
 	}
 	
 	public static void main(String[] args) {
-		ab02_SumInLoop im = new ab02_SumInLoop();
 		Scanner in = new Scanner(System.in);
-		System.out.println("input data:");
 		int size = in.nextInt();
 		int[] array = new int[size];
+		
 		for (int i=0;i<size;i++) {
 			int a = in.nextInt();
 			array[i]=a;
 		}
-		System.out.println(im.sum(array,size));
+		
+		System.out.println(sum(array,size));
 	}
 	
 }
