b68a2ab735942e6b8bf215b5506f331e1c9e8641
mdnu <mark@sdentertainment.ca>
2016-09-30 22:37:01 -0400

    Caesar Shift Cipher progress (incomplete).

--- src/ab40_CaesarShiftCipher.java
+++ src/ab40_CaesarShiftCipher.java
@@ -3,87 +3,39 @@
  */
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class ab40_CaesarShiftCipher {
 	private final static char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-	private static char[] temp;
 	public static int wordCount, lineCount;
 	
-	public static char cipher(int N, int K, char[] temp) {
-		int temp2 = K;
-		if (Character.isAlphabetic(temp[N])) {
-			//temp2 = (temp[j] - 'A') - wordCount;
+	public static char cipher(char c, int temp2) {
+		if (Character.isAlphabetic(c)) {
+			temp2 = (c - 'A') - wordCount;
 			if (temp2 < 0) {
 				temp2 += 26;
 			}
 			return (ALPHABET[temp2 % 26]);
 		} else {
-			return (temp[N]);
+			return (c);
 		}
 	}
 	
 	public static void main(String[] args) {
-		ArrayList<Character> charList = new ArrayList<Character>();
-		ArrayList<String> strList = new ArrayList<String>();
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
 		lineCount = in.nextInt();
 		wordCount = in.nextInt();
+		char c;
+		char temp[];
+		System.out.println();
 		
 		for (int i = 0; i < lineCount; i++) {
 			temp = in.nextLine().toCharArray();
+			int temp2 = 0;
 			for (int j = 0; j < temp.length; j++) {
-				charList.add(cipher(i, j, temp));
+				c = cipher(temp[j], temp2);
+				System.out.print(c);
 			}
-			System.out.print(charList + " ");
 		}
 	}
 }
-
-/**
-import java.util.Scanner;
-import java.util.ArrayList;
-
-public class ab40_CaesarShiftCipher {
-	public static char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-	public static char[] temp;
-	public static int wordCount;
-	public static int lineCount;
-	
-	public static char method(int j) {
-		int temp2;
-		if (Character.isAlphabetic(temp[j])) {
-			temp2 = (temp[j] - 'A') - wordCount;
-			if (temp2 < 0) {
-				temp2 += 26;
-			}
-			return (ALPHABET[temp2 % 26]);
-		} else {
-			return (temp[j]);
-		}
-	}
-	
-	public static void main(String[] args) {
-		ArrayList<Character> charlist = new ArrayList<Character>();
-		ArrayList<String> strlist = new ArrayList<String>();
-		@SuppressWarnings("resource")
-		Scanner in = new Scanner(System.in);
-		lineCount = in.nextInt();
-		wordCount = in.nextInt();
-		
-		for (int i = 0; i < lineCount+1; i++) {
-			temp = in.nextLine().toCharArray();
-			int temp2;
-			for (int j = 0; j < temp.length; j++) {
-				charlist.add(method(j));
-			}
-			strlist.set(0, charlist.get(0)+" ");
-		}
-		
-		for (int i = 0; i < strlist.size(); i++) {
-			System.out.print(strlist.get(i));
-		}	
-	}
-}
-*/
