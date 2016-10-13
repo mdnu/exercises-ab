1f2e589f29074927a743fe6a349df092c2858265
mdnu <mark@sdentertainment.ca>
2016-10-12 23:01:56 -0400

    Matching Brackets progress (incomplete)

--- src/ab44_MatchingBrackets.java
+++ src/ab44_MatchingBrackets.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class ab44_MatchingBrackets {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int output(String line, Deque<Character> queue) {
+		Character character;
+		boolean value = true;
+		for (int i = 0; i < line.length(); i++) {
+			character = line.charAt(i);
+		}
+		return 0;
 	}
-
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		
+		for (int i = 0; i < size; i++) {
+			Deque<Character> queue = new LinkedList<Character>();
+			String line = in.nextLine();
+		}
+	}
 }
